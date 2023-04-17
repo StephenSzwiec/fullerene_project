de8e34ec6d974a81eef6c71b482e68820f197376
Stephen Szwiec <55602870+StephenSzwiec@users.noreply.github.com>
2023-04-17 10:32:24 -0500

    Script pipelined by GNU/Parallel for docking.
    - Performs pipeline for docking using Patchdock on a single thread
    - Invoked by PBS scripted job using GNU/Parallel for vectorization support of Perl 
    - Poses generated used in next step of docking with Vina

--- thescript.sh
+++ thescript.sh
@@ -1,4 +1,19 @@
+####
+# Authored by Stephen Szwiec
+# for Rasulev Lab, North Dakota State University
+# 2022 September 06
+###
+# This script is designed be used by the pipeline to generate a list of all the
+# most likely docking poses for a given ligand.
+###
+# This script takes two arguments: $1 is the name of the receptor, $2 is the name of the ligand.
+###
+#
+# This code is licensed under the GNU General Public License v3.0
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+####
+
 #!/bin/bash
-perl buildParams.pl ./full/recp$1.pdb ./lig/$2.pdb 2.0 Default params-$1-$2.txt
+perl buildParamsFine.pl ./prot/$1.pdb ./nanoecdy/$2_*.pdb 1.0 drug params-$1-$2.txt
 ./patch_dock.Linux params-$1-$2.txt recp-$1-$2.txt
 perl transOutput.pl recp-$1-$2.txt 1 5
